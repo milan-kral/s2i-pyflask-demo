5e688351b78ed23777efe7bdcf1794eb0c32649d
milan-kral <123748257+milan-kral@users.noreply.github.com>
2023-01-27 17:19:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World ! - Pyflask Demo VERZE PRO DIVAKY MIKR</h1>'
+    return '<h1>Hello, World ! - Pyflask Demo VERZE PRO DIVAKY MIKR Verze 2</h1>'
 
 @app.route('/version')
 def get_version():
